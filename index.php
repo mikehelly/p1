03b96fc014cc9e6e9293a59ad1f085e94f79e95d
Mike Helly <mike@mikehelly.com>
2014-06-30 13:10:31 -0400

    added index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<h1>Mike Helly</h1>
+<p>My name is Mike and I am a graphic designer who is interested in learning code</p>
+<h3>Why are you taking this course?</h3>
+<p>Knowing how to code on top of making things look nice will make me the ultimate destroyer.</p>
+<h3>What previous experience do you have with HTML/CSS?</h3>
+<p>I learned how to use HTML/CSS during my sophomore year at Northeastern. With this knowledge I have worked on a few different projects, including rooftopshout.org, mhspecs.com and mikehelly.com. Currently I work for the Boston Web Group where I am responsible for editing HTML/CSS in wordpress themes.</p>
+<h3>What previous experience do you have with programming?</h3>
+<p>I have minimal work with Javascript and MySQL.</p>
+<h3>Have you worked with PHP before? If so, to what extent?</h3>
+<p>A little bit in Wordpress</p>
+<h3>Are you working on a Mac, PC, or some other platform?</h3>
+<p>MAC</p>
+<h3>Anything else we should now?</h3>
+<p>Yell at me if it seems like I'm getting lazy.</p>
