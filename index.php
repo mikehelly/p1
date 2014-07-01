f9768eba97d7a8f3603a61de2a786e8d05a66eb3
Mike Helly <mike@mikehelly.com>
2014-06-30 21:17:12 -0400

    index validation edits

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Mike Helly</title>
 	<link href="master.css" rel="stylesheet" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
