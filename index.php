1fb1d833947da100fb7724025ff34e40e7033ae7
Mike Helly <mike@mikehelly.com>
2014-06-30 20:57:40 -0400

    Updated content in index

--- index.php
+++ index.php
@@ -7,22 +7,20 @@
 	<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<img class="portrait" src="http://mikehelly.com/p1_image2.jpg" />
+<img class="portrait" src="p1_image2.jpg" />
 <div class="questions">
 <h1>Mike Helly</h1>
-<p>My name is Mike and I am a graphic designer who is interested in learning code</p>
+<p>My name is Mike and I am a graphic designer at Boston Web Group. My interests include skateboarding, typography and the Wu Tang Clan.</p>
 <h3>Why are you taking this course?</h3>
-<p>Knowing how to code on top of making things look nice will make me the ultimate destroyer.</p>
+<p>I've been interested in learning how to do some more server-side coding for awhile. I think it would be an awesome skill to have.</p>
 <h3>What previous experience do you have with HTML/CSS?</h3>
 <p>I learned how to use HTML/CSS during my sophomore year at Northeastern. With this knowledge I have worked on a few different projects, including rooftopshout.org, mhspecs.com and mikehelly.com. Currently I work for the Boston Web Group where I am responsible for editing HTML/CSS in wordpress themes.</p>
 <h3>What previous experience do you have with programming?</h3>
 <p>I have done minimal work with Javascript and MySQL.</p>
 <h3>Have you worked with PHP before? If so, to what extent?</h3>
-<p>A little bit in Wordpress</p>
+<p>No</p>
 <h3>Are you working on a Mac, PC, or some other platform?</h3>
 <p>MAC</p>
-<h3>Anything else we should know?</h3>
-<p>Yell at me if it seems like I'm getting lazy.</p>
 </div>
 </body>
 </html>
